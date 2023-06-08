778d3163c3e14e59ef4974d1f7a0990f4b1b9db7
DKO96 <danielwjkhoo@gmail.com>
2023-06-08 11:54:28 -0400

    trailbot_state topic

--- fsm/fsm/trailbot_fsm.py
+++ fsm/fsm/trailbot_fsm.py
@@ -11,22 +11,32 @@ from yasmin_viewer import YasminViewerPub
 
 
 class SearchState(State):
-    def __init__(self, blackboard):
+    def __init__(self, state_publisher, blackboard):
         super().__init__(outcomes=['target_found', 'target_not_found'])
+        self.state_publisher = state_publisher
         self.blackboard = blackboard
 
     def execute(self, blackboard):
+        state_msg = String()
+        state_msg.data = self.__class__.__name__
+        self.state_publisher.publish(state_msg)
+        
         if blackboard.get('target_found', False):
             return 'target_found'
         return 'target_not_found'
 
 class ApproachState(State):
-    def __init__(self, goal_publisher, blackboard):
+    def __init__(self, goal_publisher, state_publisher, blackboard):
         super().__init__(outcomes=['arrived', 'not_arrived'])
         self.goal_publisher = goal_publisher
+        self.state_publisher = state_publisher
         self.blackboard = blackboard
 
     def execute(self, blackboard):
+        state_msg = String()
+        state_msg.data = self.__class__.__name__
+        self.state_publisher.publish(state_msg)
+
         target_location = blackboard.get('target_location', None)
         if target_location is not None:
             self.goal_publisher.publish(target_location)
@@ -36,21 +46,31 @@ class ApproachState(State):
         return 'not_arrived'
 
 class WaitState(State):
-    def __init__(self, blackboard):
+    def __init__(self, state_publisher, blackboard):
         super().__init__(outcomes=['snack_dispensed', 'snack_not_dispensed'])
+        self.state_publisher = state_publisher
         self.blackboard = blackboard
 
     def execute(self, blackboard):
+        state_msg = String()
+        state_msg.data = self.__class__.__name__
+        self.state_publisher.publish(state_msg)
+        
         if blackboard.get('snack_dispensed', False):
             return 'snack_dispensed'
         return 'snack_not_dispensed'
 
 class DoneState(State):
-    def __init__(self, blackboard):
+    def __init__(self, state_publisher, blackboard):
         super().__init__(outcomes=['done'])
+        self.state_publisher = state_publisher
         self.blackboard = blackboard
 
     def execute(self, blackboard):
+        state_msg = String()
+        state_msg.data = self.__class__.__name__
+        self.state_publisher.publish(state_msg)
+        
         blackboard['target_found'] = False
         blackboard['snack_dispensed'] = False
         return 'done'
@@ -61,6 +81,7 @@ class FSM(Node):
 
         self.blackboard = {}
 
+        self.state_publisher = self.create_publisher(String, 'trailbot_state', 10)
         self.goal_publisher = self.create_publisher(PoseStamped, 'goal_pose', 10)
         self.goal_subscriber = self.create_subscription(
             String,
@@ -83,10 +104,10 @@ class FSM(Node):
 
         sm = StateMachine(outcomes=['finished'])
 
-        sm.add_state('SEARCH', SearchState(self.blackboard), transitions={'target_found': 'APPROACH', 'target_not_found': 'SEARCH'})
-        sm.add_state('APPROACH', ApproachState(self.goal_publisher,self.blackboard), transitions={'arrived': 'WAIT', 'not_arrived': 'SEARCH'})
-        sm.add_state('WAIT', WaitState(self.blackboard), transitions={'snack_dispensed': 'DONE', 'snack_not_dispensed': 'WAIT'})
-        sm.add_state('DONE', DoneState(self.blackboard), transitions={'done': 'SEARCH'})
+        sm.add_state('SEARCH', SearchState(self.state_publisher, self.blackboard), transitions={'target_found': 'APPROACH', 'target_not_found': 'SEARCH'})
+        sm.add_state('APPROACH', ApproachState(self.goal_publisher,self.state_publisher, self.blackboard), transitions={'arrived': 'WAIT', 'not_arrived': 'SEARCH'})
+        sm.add_state('WAIT', WaitState(self.state_publisher, self.blackboard), transitions={'snack_dispensed': 'DONE', 'snack_not_dispensed': 'WAIT'})
+        sm.add_state('DONE', DoneState(self.state_publisher, self.blackboard), transitions={'done': 'SEARCH'})
 
         outcome = sm.execute(self.blackboard)
         print(outcome)
