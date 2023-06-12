5d688e317c0391a5ba31694c335aa87a0e0f4761
trailbot <nic.koenig37@gmail.com>
2023-06-12 17:46:21 -0400

    changes

--- husky/husky_base/launch/occupancy_grid.launch.py
+++ husky/husky_base/launch/occupancy_grid.launch.py
@@ -32,4 +32,31 @@ def generate_launch_description():
             output='screen',
             parameters=[{'use_sim_time': use_sim_time}],
             arguments=['-resolution', resolution, '-publish_period_sec', publish_period_sec]),
+        
+        #pointcloud to laserscan conversion node
+        Node(
+            package='pointcloud_to_laserscan_converter', executable='pointcloud_to_laserscan_node',
+            remappings=[('points2', '/velodyne_points'),],
+            parameters=[{
+                # 'target_frame': 'base_link',
+                'target_frame': 'laser_frame',
+                'transform_tolerance': 0.01,
+                #base link height max/min
+                # 'min_height': 0.1, #4 inches
+                # 'max_height': 1.0,
+                #laser_frame height max/min
+                'min_height': -0.9282, #4 inches above ground 
+                'max_height': 0.1, 
+                'angle_min': -3.14,  #was -M_PI/2 now -PI
+                'angle_max': 3.14,  #was  M_PI/2 now PI
+                'angle_increment': 0.0087,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.45,
+                'range_max': 30.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
+        ),
+
     ])
