8883f311fd703abbcb140a3a9d93888f1aae4c66
koener <nic.koenig37@gmail.com>
2023-06-12 21:51:41 -0400

    IMU FILES

--- husky/husky_base/config/trailbot_lds_3d.lua
+++ husky/husky_base/config/trailbot_lds_3d.lua
@@ -28,7 +28,7 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
-TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 160
+TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
 
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
