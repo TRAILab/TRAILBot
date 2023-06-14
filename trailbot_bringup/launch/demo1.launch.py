1e75a8488d21429a03de4b908c2d756482840b69
FrankZhaodong <tom2352759619@hotmail.com>
2023-06-14 11:31:56 -0400

    add logging to launch file

--- trailbot_bringup/launch/demo1.launch.py
+++ trailbot_bringup/launch/demo1.launch.py
@@ -2,12 +2,14 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (Command, FindExecutable, LaunchConfiguration,
                                   PathJoinSubstitution)
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from datetime import date
+from datetime import datetime
 
 
 def generate_launch_description():
@@ -199,6 +201,15 @@ def generate_launch_description():
         executable='human_detection_node'
     )
 
+    # Launch file logging
+    current_date = date.today()
+    current_time = datetime.now().time()
+    formatted_time = current_time.strftime("%H:%M")
+    file_logging = ExecuteProcess(
+        cmd=['ros2', 'bag', 'record', '-o', f'/home/trailbot/bags/{current_date}-{formatted_time}','-a','-x','/camera'],
+        output='screen'
+    )
+
     ld = LaunchDescription()
     ld.add_action(node_robot_state_publisher)
     ld.add_action(node_controller_manager)
@@ -219,5 +230,6 @@ def generate_launch_description():
     ld.add_action(ximea_node)
     ld.add_action(launch_voice_assistant)
     ld.add_action(human_detection_node)
+    ld.add_action(file_logging)
 
     return ld
