c860d3e8ca960eb6297c4b479254cd9e21a8af05
Frank_ZhaodongJiang <69261064+FrankZhaodong@users.noreply.github.com>
2023-06-29 16:17:10 -0400

    Add a flag for data logging

--- husky/husky_base/launch/base.launch.py
+++ husky/husky_base/launch/base.launch.py
@@ -212,6 +212,9 @@ def generate_launch_description():
     #ld.add_action(IMU_node)
     #ld.add_action(PCL2SCAN)
     ld.add_action(ximea_node)
-    ld.add_action(file_logging)
+
+    file_logging = False
+    if file_logging:
+        ld.add_action(file_logging)
 
     return ld
