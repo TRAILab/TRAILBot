af8654a5e0cc3fdf1f1e021425efecc36d343a94
Nick <nic.koenig37@gmail.com>
2023-07-10 11:29:15 -0400

    Folder for all mapping files and utilities

---
+++ SLAM/launch/occupancy_grid.launch.py
@@ -0,0 +1,63 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    resolution = LaunchConfiguration('resolution', default='0.05')
+    publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'resolution',
+            default_value=resolution,
+            description='Resolution of a grid cell in the published occupancy grid'),
+
+        DeclareLaunchArgument(
+            'publish_period_sec',
+            default_value=publish_period_sec,
+            description='OccupancyGrid publishing period'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_occupancy_grid_node',
+            name='occupancy_grid_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-resolution', resolution, '-publish_period_sec', publish_period_sec]),
+        
+        #pointcloud to laserscan conversion node
+        Node(
+            package='pointcloud_to_laserscan_converter', executable='pointcloud_to_laserscan_node',
+            remappings=[('points2', '/velodyne_points'),],
+            parameters=[{
+                # 'target_frame': 'base_link',
+                # 'target_frame': 'laser_frame',
+                'target_frame': 'velodyne',
+                'transform_tolerance': 0.01,
+                #base link height max/min
+                # 'min_height': 0.1, #4 inches
+                # 'max_height': 1.0,
+                #laser_frame height max/min
+                'min_height': -0.9282, #4 inches above ground 
+                'max_height': 0.1,
+                'angle_min': -3.14,  #was -M_PI/2 now -PI
+                'angle_max': 3.14,  #was  M_PI/2 now PI
+                'angle_increment': 0.0087,  # M_PI/360.0
+                'scan_time': 0.1, #10 hz (was at .3333)
+                'range_min': 0.45,
+                'range_max': 30.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
+        ),
+
+    ])
---
+++ SLAM/setup.py
@@ -0,0 +1,33 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'SLAM'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*_launch.py')),
+        
+        #Include all config files
+        (os.path.join('share', package_name, 'config'), glob('config/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nick',
+    maintainer_email='koenin1@mcmaster.ca',
+    description='Package to launch mapping utilities',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
