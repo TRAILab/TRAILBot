9df68edb0fad2b87dca83d8971ef01bfdf4169af
nickoenig37 <79549466+nickoenig37@users.noreply.github.com>
2023-07-17 17:14:57 -0400

    Creating separate nav and slam folders  (#36)
    * Folder for only Navigation files and utilities
    
    * Folder for all mapping files and utilities
    
    * Deleting nav and slam files from husky_base
    
    * Had to rename 'SLAM' Folder to 'slam' for xml rule
    
    * Removing pcl2laserscan package from husky

---
+++ slam/launch/trailbot_cartographer.launch.py
@@ -0,0 +1,51 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    trailbot_cartographer_prefix = get_package_share_directory('trailbot_gazebo')
+    cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default=os.path.join(
+                                                  trailbot_cartographer_prefix, 'config'))
+    configuration_basename = LaunchConfiguration('configuration_basename',
+                                                  default='trailbot_lds_2d.lua')
+
+    resolution = LaunchConfiguration('resolution', default='0.05')
+    publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
+
+    rviz_config_dir = os.path.join(get_package_share_directory('trailbot_gazebo'),
+                                   'config','rviz_config.rviz')
+
+    return LaunchDescription([
+
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_node',
+            name='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-configuration_directory', cartographer_config_dir,
+                       '-configuration_basename', configuration_basename]),
+                       
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/occupancy_grid.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time, 'resolution': resolution,
+                              'publish_period_sec': publish_period_sec}.items(),
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+    ])
