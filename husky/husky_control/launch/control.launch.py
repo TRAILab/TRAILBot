da61680e028b177cd8c450d499a7c60b64d16711
Nick <nic.koenig37@gmail.com>
2023-08-08 17:01:15 -0400

    Commenting out the ekf node

--- husky/husky_control/launch/control.launch.py
+++ husky/husky_control/launch/control.launch.py
@@ -14,14 +14,14 @@ def generate_launch_description():
         'localization.yaml'],
     )
 
-    node_ekf = Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_node',
-        output='screen',
-        parameters=[config_husky_ekf],
-        )
-    ld.add_action(node_ekf)
+    # node_ekf = Node(
+    #     package='robot_localization',
+    #     executable='ekf_node',
+    #     name='ekf_node',
+    #     output='screen',
+    #     parameters=[config_husky_ekf],
+    #     )
+    # ld.add_action(node_ekf)
 
     primary_imu_enable = EnvironmentVariable('CPR_IMU', default_value='false')
 
