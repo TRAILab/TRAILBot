1b67098aa6690b97f8fa4667d48257f85ca6366a
Nathan <nathanh4903@gmail.com>
2023-08-28 21:45:57 +0800

    Update human_detection_node.py

--- human_detection/yolov7_skeleton/human_detection_node.py
+++ human_detection/yolov7_skeleton/human_detection_node.py
@@ -249,7 +249,7 @@ class LidarCameraSubscriber(Node):
                     cv2.circle(image_with_dots, (int(person.x), int(person.y)), 5, (0, 0, 255), -1)  # Draw a red circle at (x, y)
                 
                 cv2.imshow("Camera Image", image_with_dots)
-                cv2.waitKey(0)
+                cv2.waitKey(1)
             except:
                 pass
 
