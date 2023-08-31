efd93a159fb62fd79194cfa2e0d93fe21319a426
Frank_ZhaodongJiang <69261064+FrankZhaodong@users.noreply.github.com>
2023-08-31 16:33:30 -0400

    add psp model, change the coordinate conversion

--- trail_detection_node/trail_detection_node/model_loader.py
+++ trail_detection_node/trail_detection_node/model_loader.py
@@ -2,6 +2,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from .vgg import vgg16
 
+import torch
+from .segbase import SegBaseModel
+
 class FCN8s(nn.Module):
     def __init__(self, nclass, backbone='vgg16', aux=False, pretrained_base=True, norm_layer=nn.BatchNorm2d, **kwargs):
         super(FCN8s, self).__init__()
@@ -97,3 +100,94 @@ class _FCNHead(nn.Module):
 
     def forward(self, x):
         return self.block(x)
+
+'''
+PSP NET
+'''   
+
+def _PSP1x1Conv(in_channels, out_channels, norm_layer, norm_kwargs):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 1, bias=False),
+        norm_layer(out_channels, **({} if norm_kwargs is None else norm_kwargs)),
+        nn.ReLU(True)
+    )
+
+class _PyramidPooling(nn.Module):
+    def __init__(self, in_channels, **kwargs):
+        super(_PyramidPooling, self).__init__()
+        out_channels = int(in_channels / 4)
+        self.avgpool1 = nn.AdaptiveAvgPool2d(1)
+        self.avgpool2 = nn.AdaptiveAvgPool2d(2)
+        self.avgpool3 = nn.AdaptiveAvgPool2d(3)
+        self.avgpool4 = nn.AdaptiveAvgPool2d(6)
+        self.conv1 = _PSP1x1Conv(in_channels, out_channels, **kwargs)
+        self.conv2 = _PSP1x1Conv(in_channels, out_channels, **kwargs)
+        self.conv3 = _PSP1x1Conv(in_channels, out_channels, **kwargs)
+        self.conv4 = _PSP1x1Conv(in_channels, out_channels, **kwargs)
+
+    def forward(self, x):
+        size = x.size()[2:]
+        feat1 = F.interpolate(self.conv1(self.avgpool1(x)), size, mode='bilinear', align_corners=True)
+        feat2 = F.interpolate(self.conv2(self.avgpool2(x)), size, mode='bilinear', align_corners=True)
+        feat3 = F.interpolate(self.conv3(self.avgpool3(x)), size, mode='bilinear', align_corners=True)
+        feat4 = F.interpolate(self.conv4(self.avgpool4(x)), size, mode='bilinear', align_corners=True)
+        return torch.cat([x, feat1, feat2, feat3, feat4], dim=1)
+
+class _PSPHead(nn.Module):
+    def __init__(self, nclass, norm_layer=nn.BatchNorm2d, norm_kwargs=None, **kwargs):
+        super(_PSPHead, self).__init__()
+        self.psp = _PyramidPooling(2048, norm_layer=norm_layer, norm_kwargs=norm_kwargs)
+        self.block = nn.Sequential(
+            nn.Conv2d(4096, 512, 3, padding=1, bias=False),
+            norm_layer(512, **({} if norm_kwargs is None else norm_kwargs)),
+            nn.ReLU(True),
+            nn.Dropout(0.1),
+            nn.Conv2d(512, nclass, 1)
+        )
+
+    def forward(self, x):
+        x = self.psp(x)
+        return self.block(x)
+
+class PSPNet(SegBaseModel):
+    r"""Pyramid Scene Parsing Network
+
+    Parameters
+    ----------
+    nclass : int
+        Number of categories for the training dataset.
+    backbone : string
+        Pre-trained dilated backbone network type (default:'resnet50'; 'resnet50',
+        'resnet101' or 'resnet152').
+    norm_layer : object
+        Normalization layer used in backbone network (default: :class:`nn.BatchNorm`;
+        for Synchronized Cross-GPU BachNormalization).
+    aux : bool
+        Auxiliary loss.
+
+    Reference:
+        Zhao, Hengshuang, Jianping Shi, Xiaojuan Qi, Xiaogang Wang, and Jiaya Jia.
+        "Pyramid scene parsing network." *CVPR*, 2017
+    """
+
+    def __init__(self, nclass, backbone='resnet50', aux=False, pretrained_base=True, **kwargs):
+        super(PSPNet, self).__init__(nclass, aux, backbone, pretrained_base=pretrained_base, **kwargs)
+        self.head = _PSPHead(nclass, **kwargs)
+        if self.aux:
+            self.auxlayer = _FCNHead(1024, nclass, **kwargs)
+
+        self.__setattr__('exclusive', ['head', 'auxlayer'] if aux else ['head'])
+
+    def forward(self, x):
+        size = x.size()[2:]
+        _, _, c3, c4 = self.base_forward(x)
+        outputs = []
+        x = self.head(c4)
+        x = F.interpolate(x, size, mode='bilinear', align_corners=True)
+        outputs.append(x)
+
+        if self.aux:
+            auxout = self.auxlayer(c3)
+            auxout = F.interpolate(auxout, size, mode='bilinear', align_corners=True)
+            outputs.append(auxout)
+        return tuple(outputs)
