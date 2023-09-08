7e21927f85bfab9068047782c082ef4dcf61fad3
Frank_ZhaodongJiang <69261064+FrankZhaodong@users.noreply.github.com>
2023-09-07 20:17:46 -0400

    merged all the model loading part into one file

--- trail_detection_node/trail_detection_node/model_loader.py
+++ trail_detection_node/trail_detection_node/model_loader.py
@@ -1,90 +1,310 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from .vgg import vgg16
 
 import torch
-from .segbase import SegBaseModel
+import os
 
-class FCN8s(nn.Module):
-    def __init__(self, nclass, backbone='vgg16', aux=False, pretrained_base=True, norm_layer=nn.BatchNorm2d, **kwargs):
-        super(FCN8s, self).__init__()
-        self.aux = aux
-        if backbone == 'vgg16':
-            self.pretrained = vgg16(pretrained=pretrained_base).features
-        else:
-            raise RuntimeError('unknown backbone: {}'.format(backbone))
-        self.pool3 = nn.Sequential(*self.pretrained[:17])
-        self.pool4 = nn.Sequential(*self.pretrained[17:24])
-        self.pool5 = nn.Sequential(*self.pretrained[24:])
-        self.head = _FCNHead(512, nclass, norm_layer)
-        self.score_pool3 = nn.Conv2d(256, nclass, 1)
-        self.score_pool4 = nn.Conv2d(512, nclass, 1)
-        if aux:
-            self.auxlayer = _FCNHead(512, nclass, norm_layer)
+# all the code below are copied from https://github.com/Tramac/awesome-semantic-segmentation-pytorch
 
-        self.__setattr__('exclusive',
-                         ['head', 'score_pool3', 'score_pool4', 'auxlayer'] if aux else ['head', 'score_pool3',
-                                                                                         'score_pool4'])
+class BasicBlockV1b(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, dilation=1, downsample=None,
+                 previous_dilation=1, norm_layer=nn.BatchNorm2d):
+        super(BasicBlockV1b, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, 3, stride,
+                               dilation, dilation, bias=False)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(True)
+        self.conv2 = nn.Conv2d(planes, planes, 3, 1, previous_dilation,
+                               dilation=previous_dilation, bias=False)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
 
     def forward(self, x):
-        pool3 = self.pool3(x)
-        pool4 = self.pool4(pool3)
-        pool5 = self.pool5(pool4)
+        identity = x
 
-        outputs = []
-        score_fr = self.head(pool5)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-        score_pool4 = self.score_pool4(pool4)
-        score_pool3 = self.score_pool3(pool3)
+        out = self.conv2(out)
+        out = self.bn2(out)
 
-        upscore2 = F.interpolate(score_fr, score_pool4.size()[2:], mode='bilinear', align_corners=True)
-        fuse_pool4 = upscore2 + score_pool4
+        if self.downsample is not None:
+            identity = self.downsample(x)
 
-        upscore_pool4 = F.interpolate(fuse_pool4, score_pool3.size()[2:], mode='bilinear', align_corners=True)
-        fuse_pool3 = upscore_pool4 + score_pool3
+        out += identity
+        out = self.relu(out)
 
-        out = F.interpolate(fuse_pool3, x.size()[2:], mode='bilinear', align_corners=True)
-        outputs.append(out)
+        return out
 
-        if self.aux:
-            auxout = self.auxlayer(pool5)
-            auxout = F.interpolate(auxout, x.size()[2:], mode='bilinear', align_corners=True)
-            outputs.append(auxout)
 
-        return tuple(outputs)
+class BottleneckV1b(nn.Module):
+    expansion = 4
 
-class FCN32s(nn.Module):
-    """There are some difference from original fcn"""
-
-    def __init__(self, nclass, backbone='vgg16', aux=False, pretrained_base=True,
-                 norm_layer=nn.BatchNorm2d, **kwargs):
-        super(FCN32s, self).__init__()
-        self.aux = aux
-        if backbone == 'vgg16':
-            self.pretrained = vgg16(pretrained=pretrained_base).features
-        else:
-            raise RuntimeError('unknown backbone: {}'.format(backbone))
-        self.head = _FCNHead(512, nclass, norm_layer)
-        if aux:
-            self.auxlayer = _FCNHead(512, nclass, norm_layer)
-
-        self.__setattr__('exclusive', ['head', 'auxlayer'] if aux else ['head'])
+    def __init__(self, inplanes, planes, stride=1, dilation=1, downsample=None,
+                 previous_dilation=1, norm_layer=nn.BatchNorm2d):
+        super(BottleneckV1b, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, 1, bias=False)
+        self.bn1 = norm_layer(planes)
+        self.conv2 = nn.Conv2d(planes, planes, 3, stride,
+                               dilation, dilation, bias=False)
+        self.bn2 = norm_layer(planes)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, 1, bias=False)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(True)
+        self.downsample = downsample
+        self.stride = stride
 
     def forward(self, x):
-        size = x.size()[2:]
-        pool5 = self.pretrained(x)
+        identity = x
 
-        outputs = []
-        out = self.head(pool5)
-        out = F.interpolate(out, size, mode='bilinear', align_corners=True)
-        outputs.append(out)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class ResNetV1b(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, dilated=True, deep_stem=False,
+                 zero_init_residual=False, norm_layer=nn.BatchNorm2d):
+        self.inplanes = 128 if deep_stem else 64
+        super(ResNetV1b, self).__init__()
+        if deep_stem:
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(3, 64, 3, 2, 1, bias=False),
+                norm_layer(64),
+                nn.ReLU(True),
+                nn.Conv2d(64, 64, 3, 1, 1, bias=False),
+                norm_layer(64),
+                nn.ReLU(True),
+                nn.Conv2d(64, 128, 3, 1, 1, bias=False)
+            )
+        else:
+            self.conv1 = nn.Conv2d(3, 64, 7, 2, 3, bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(True)
+        self.maxpool = nn.MaxPool2d(3, 2, 1)
+        self.layer1 = self._make_layer(block, 64, layers[0], norm_layer=norm_layer)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, norm_layer=norm_layer)
+        if dilated:
+            self.layer3 = self._make_layer(block, 256, layers[2], stride=1, dilation=2, norm_layer=norm_layer)
+            self.layer4 = self._make_layer(block, 512, layers[3], stride=1, dilation=4, norm_layer=norm_layer)
+        else:
+            self.layer3 = self._make_layer(block, 256, layers[2], stride=2, norm_layer=norm_layer)
+            self.layer4 = self._make_layer(block, 512, layers[3], stride=2, norm_layer=norm_layer)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, BottleneckV1b):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlockV1b):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1, norm_layer=nn.BatchNorm2d):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, 1, stride, bias=False),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        if dilation in (1, 2):
+            layers.append(block(self.inplanes, planes, stride, dilation=1, downsample=downsample,
+                                previous_dilation=dilation, norm_layer=norm_layer))
+        elif dilation == 4:
+            layers.append(block(self.inplanes, planes, stride, dilation=2, downsample=downsample,
+                                previous_dilation=dilation, norm_layer=norm_layer))
+        else:
+            raise RuntimeError("=> unknown dilation size: {}".format(dilation))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, dilation=dilation,
+                                previous_dilation=dilation, norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+_model_sha1 = {name: checksum for checksum, name in [
+    ('25c4b50959ef024fcc050213a06b614899f94b3d', 'resnet50'),
+    ('2a57e44de9c853fa015b172309a1ee7e2d0e4e2a', 'resnet101'),
+    ('0d43d698c66aceaa2bc0309f55efdd7ff4b143af', 'resnet152'),
+]}
+
+def short_hash(name):
+    if name not in _model_sha1:
+        raise ValueError('Pretrained model for {name} is not available.'.format(name=name))
+    return _model_sha1[name][:8]
+
+def get_resnet_file(name, root='~/.torch/models'):
+    file_name = '{name}-{short_hash}'.format(name=name, short_hash=short_hash(name))
+    root = os.path.expanduser(root)
+
+    file_path = os.path.join(root, file_name + '.pth')
+    sha1_hash = _model_sha1[name]
+    if os.path.exists(file_path):
+        return file_path
+
+    else:
+        print('Model file {} is not found.'.format(file_path))
+
+def resnet50_v1s(pretrained=False, root='~/.torch/models', **kwargs):
+    model = ResNetV1b(BottleneckV1b, [3, 4, 6, 3], deep_stem=True, **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(get_resnet_file('resnet50', root=root)), strict=False)
+    return model
+
+class SeparableConv2d(nn.Module):
+    def __init__(self, inplanes, planes, kernel_size=3, stride=1, padding=1,
+                 dilation=1, bias=False, norm_layer=nn.BatchNorm2d):
+        super(SeparableConv2d, self).__init__()
+        self.conv = nn.Conv2d(inplanes, inplanes, kernel_size, stride, padding, dilation, groups=inplanes, bias=bias)
+        self.bn = norm_layer(inplanes)
+        self.pointwise = nn.Conv2d(inplanes, planes, 1, bias=bias)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.pointwise(x)
+        return x
+
+# copy from: https://github.com/wuhuikai/FastFCN/blob/master/encoding/nn/customize.py
+class JPU(nn.Module):
+    def __init__(self, in_channels, width=512, norm_layer=nn.BatchNorm2d, **kwargs):
+        super(JPU, self).__init__()
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels[-1], width, 3, padding=1, bias=False),
+            norm_layer(width),
+            nn.ReLU(True))
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels[-2], width, 3, padding=1, bias=False),
+            norm_layer(width),
+            nn.ReLU(True))
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels[-3], width, 3, padding=1, bias=False),
+            norm_layer(width),
+            nn.ReLU(True))
+
+        self.dilation1 = nn.Sequential(
+            SeparableConv2d(3 * width, width, 3, padding=1, dilation=1, bias=False),
+            norm_layer(width),
+            nn.ReLU(True))
+        self.dilation2 = nn.Sequential(
+            SeparableConv2d(3 * width, width, 3, padding=2, dilation=2, bias=False),
+            norm_layer(width),
+            nn.ReLU(True))
+        self.dilation3 = nn.Sequential(
+            SeparableConv2d(3 * width, width, 3, padding=4, dilation=4, bias=False),
+            norm_layer(width),
+            nn.ReLU(True))
+        self.dilation4 = nn.Sequential(
+            SeparableConv2d(3 * width, width, 3, padding=8, dilation=8, bias=False),
+            norm_layer(width),
+            nn.ReLU(True))
+
+    def forward(self, *inputs):
+        feats = [self.conv5(inputs[-1]), self.conv4(inputs[-2]), self.conv3(inputs[-3])]
+        size = feats[-1].size()[2:]
+        feats[-2] = F.interpolate(feats[-2], size, mode='bilinear', align_corners=True)
+        feats[-3] = F.interpolate(feats[-3], size, mode='bilinear', align_corners=True)
+        feat = torch.cat(feats, dim=1)
+        feat = torch.cat([self.dilation1(feat), self.dilation2(feat), self.dilation3(feat), self.dilation4(feat)],
+                         dim=1)
+
+        return inputs[0], inputs[1], inputs[2], feat
+
+
+
+class SegBaseModel(nn.Module):
+    r"""Base Model for Semantic Segmentation
+
+    Parameters
+    ----------
+    backbone : string
+        Pre-trained dilated backbone network type (default:'resnet50'; 'resnet50',
+        'resnet101' or 'resnet152').
+    """
+
+    def __init__(self, nclass, aux, backbone='resnet50', jpu=False, pretrained_base=True, **kwargs):
+        super(SegBaseModel, self).__init__()
+        dilated = False if jpu else True
+        self.aux = aux
+        self.nclass = nclass
+        if backbone == 'resnet50':
+            self.pretrained = resnet50_v1s(pretrained=pretrained_base, dilated=dilated, **kwargs)
+        else:
+            raise RuntimeError('unknown backbone: {}'.format(backbone))
+
+        self.jpu = JPU([512, 1024, 2048], width=512, **kwargs) if jpu else None
+
+    def base_forward(self, x):
+        """forwarding pre-trained network"""
+        x = self.pretrained.conv1(x)
+        x = self.pretrained.bn1(x)
+        x = self.pretrained.relu(x)
+        x = self.pretrained.maxpool(x)
+        c1 = self.pretrained.layer1(x)
+        c2 = self.pretrained.layer2(c1)
+        c3 = self.pretrained.layer3(c2)
+        c4 = self.pretrained.layer4(c3)
+
+        if self.jpu:
+            return self.jpu(c1, c2, c3, c4)
+        else:
+            return c1, c2, c3, c4
+
+    def evaluate(self, x):
+        """evaluating network with inputs and targets"""
+        return self.forward(x)[0]
+
+    def demo(self, x):
+        pred = self.forward(x)
         if self.aux:
-            auxout = self.auxlayer(pool5)
-            auxout = F.interpolate(auxout, size, mode='bilinear', align_corners=True)
-            outputs.append(auxout)
-
-        return tuple(outputs)
+            pred = pred[0]
+        return pred
 
 class _FCNHead(nn.Module):
     def __init__(self, in_channels, channels, norm_layer=nn.BatchNorm2d, **kwargs):
