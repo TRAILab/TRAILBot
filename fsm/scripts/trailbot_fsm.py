e71f233e50d8b445a27bc4b1f3815b651fb5a606
DKO96 <danielwjkhoo@gmail.com>
2024-02-15 16:44:41 -0500

    get target

--- fsm/scripts/trailbot_fsm.py
+++ fsm/scripts/trailbot_fsm.py
@@ -14,38 +14,99 @@ from robot_navigator import BasicNavigator, TaskResult
 from nav2_msgs.action import NavigateToPose
 
 
+# class SearchState(State):
+#   def __init__(self, state_publisher, blackboard):
+#     super().__init__(outcomes=["target_found", "target_not_found"])
+#     self.state_publisher = state_publisher
+#     self.blackboard = blackboard
+
+#   def execute(self, blackboard):
+#     state_msg = String()
+#     state_msg.data = self.__class__.__name__
+#     self.state_publisher.publish(state_msg)
+
+#     # check if the target has been found
+#     if blackboard.get("target_found", False):
+#       return "target_found"
+#     return "target_not_found"
+
+
+
+
+
+
+
+
 class FSM(Node):
   def __init__(self):
     super().__init__("trailbot_state_machine")
     self.nav = BasicNavigator()
-    
+
+    # create state machine and blackboard
+    # sm = StateMachine(outcomes=["finished"])
+    self.blackboard = {} 
+    # self.current_state = "SEARCH"
+
+    # add states
+    # self.sm.add_state("SEARCH", Search)
+
+    # publish trailbot state
+    self.state_publisher_ = self.create_publisher(String, 'trailbot_state', 10)
+
+    # subscribe to goal pose topic
+    self.goal_subscriber_ = self.create_subscription(PoseStamped, "goal_pose", self.goal_callback,10)
+
+    # initialize goal flag
+    self.goal_received = False
+
+
     self.nav_target()
+    
+
+
 
   def nav_target(self):
-    goal_pose = PoseStamped()
-    goal_pose.header.frame_id = "map"
-    goal_pose.pose.position.x = 3.0
-    goal_pose.pose.position.y = -3.0
-    goal_pose.pose.position.z = 0.0
-    goal_pose.pose.orientation.w = 1.0
+    # wait for goal pose message
+    while not self.goal_received:
+      print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] Waiting for goal pose.")
+    
+    # reset flag for future goals
+    self.goal_received = False
 
-    self.nav.goToPose(goal_pose)
+    # proceed with navigation if target found
+    target_pose = self.blackboard.get("target_location")
+    if target_pose:
+      self.nav.goToPose(target_pose)
 
     while not self.nav.isTaskComplete():
-      print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] {self.nav.isTaskComplete()=}")
       feedback = self.nav.getFeedback()
+      # print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] {feedback.distance_remaining=}")
+      msg = String()
+      msg.data = f"[{datetime.datetime.now().strftime('%H:%M:%S')}] APPROACH STATE"
+      self.state_publisher_.publish(msg)
 
     print(f"{self.nav.isTaskComplete()=}")
     result = self.nav.getResult()
     if result == TaskResult.SUCCEEDED:
       print("Goal succeeded!")
-    elif result == TaskResult.CANCELED:
-      print("Goal was canceled!")
-    elif result == TaskResult.FAILED:
-      print("Goal failed!")
+      msg = String()
+      msg.data = f"[{datetime.datetime.now().strftime('%H:%M:%S')}] MOVE TO NEXT STATE"
+      self.state_publisher_.publish(msg)
 
-  def _feedback(self):
-    pass
+
+  def goal_callback(self, msg):
+    self.blackboard["target_location"] = msg
+    self.blackboard["target_found"] = True
+    self.goal_received = True
+
+    # print blackboard
+    self.get_blackboard()
+
+  def get_blackboard(self):
+    print(f"Current Blackboard Contents")
+    for key, val in self.blackboard.items():
+      print(f"{key}: {val}")
+
 
 def main():
   rclpy.init()
