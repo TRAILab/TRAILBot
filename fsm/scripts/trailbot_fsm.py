a7687b442c507869e0684537e157bf402428fd83
DKO96 <danielwjkhoo@gmail.com>
2024-03-07 15:04:02 -0500

    trailbot fsm update

--- fsm/scripts/trailbot_fsm.py
+++ fsm/scripts/trailbot_fsm.py
@@ -16,13 +16,13 @@ from robot_navigator import BasicNavigator, TaskResult
 class SearchState(State):
   def __init__(self, blackboard, state_publisher):
     super().__init__(outcomes=["target_found", "target_not_found"])
-    self.state_publisher = state_publisher
+    self.state_publisher_ = state_publisher
     self.blackboard = blackboard
 
   def execute(self, blackboard):
     state_msg = String()
     state_msg.data = self.__class__.__name__
-    self.state_publisher.publish(state_msg)
+    self.state_publisher_.publish(state_msg)
 
     # check if the target has been found
     if blackboard.get("target_found", False):
@@ -33,41 +33,52 @@ class ApproachState(State):
   def __init__(self, blackboard, state_publisher):
     super().__init__(outcomes=["arrived", "not_arrived"])
     self.blackboard = blackboard
-    self.state_publisher = state_publisher
+    self.state_publisher_ = state_publisher
 
   def execute(self, blackboard):
     state_msg = String()
     state_msg.data = self.__class__.__name__
-    self.state_publisher.publish(state_msg)
+    self.state_publisher_.publish(state_msg)
 
+    target_location = blackboard.get('target_location', None)
+
+class QueryState(State):
+  def __init__(self, blackboard, state_publisher):
+    super().__init__(outcomes=["snack_dispensed", "snack_not_dispensed"])
+    self.blackboard = blackboard
+    self.state_publisher_ = state_publisher
+
+  def execute(self, blackboard):
+    state_msg = String()
+    state_msg.data = self.__class__.__name__
+    self.state_publisher_.publish(state_msg)
+
+    if blackboard.get("snack_dispensed", False):
+      return "snack_dispensed"
+    return "snack_not_dispensed"
     
+class DoneState(State):
+  def __init__(self, blackboard, state_publisher):
+    super().__init__(outcomes=["finished"])
+    self.blackboard = blackboard
+    self.state_publisher_ = state_publisher
 
+  def execute(self, blackboard):
+    state_msg = String()
+    state_msg.data = self.__class__.__name__
+    self.state_publisher_.publish(state_msg)
 
-
-
-
+    blackboard["target_found"] = False
+    blackboard["goal_reached"] = False
+    blackboard["snack_dispensed"] = False
+    return "finished"
 
 
 class FSM(Node):
   def __init__(self):
     super().__init__("trailbot_state_machine")
     self.nav = BasicNavigator()
-
-    # create state machine and blackboard
-    sm = StateMachine(outcomes=["finished"])
-    self.blackboard = {} 
-    self.current_state = "SEARCH"
-
-    # add states
-    self.sm.add_state("SEARCH", SearchState(self.state_publisher_, self.blackboard),
-                      transitions={"target_found": "APPROACH", "target_not_found": "SEARCH"})
-    self.sm.add_state("APPROACH", ApproachState(self.state_publisher_, self.blackboard),
-                      transitions={"target_found": "APPROACH", "target_not_found": "SEARCH"})
-    self.sm.add_state("SEARCH", SearchState(self.state_publisher_, self.blackboard),
-                      transitions={"target_found": "APPROACH", "target_not_found": "SEARCH"})
-    self.sm.add_state("SEARCH", SearchState(self.state_publisher_, self.blackboard),
-                      transitions={"target_found": "APPROACH", "target_not_found": "SEARCH"})
-
+    
     # publish trailbot state
     self.state_publisher_ = self.create_publisher(String, 'trailbot_state', 10)
 
@@ -77,39 +88,59 @@ class FSM(Node):
     # initialize goal flag
     self.goal_received = False
 
+    # create state machine (yasmin) and blackboard (dict)
+    self.sm = StateMachine(outcomes=["finished"])
+    self.blackboard = {} 
+    self.current_state = "SEARCH"
 
-    self.nav_target()
+    # add states
+    self.sm.add_state("SEARCH", SearchState(self.state_publisher_, self.blackboard),
+                      transitions={"target_found": "APPROACH", "target_not_found": "SEARCH"})
+    self.sm.add_state("APPROACH", ApproachState(self.state_publisher_, self.blackboard),
+                      transitions={"arrived": "QUERY", "not_arrived": "APPROACH"})
+    self.sm.add_state("QUERY", QueryState(self.state_publisher_, self.blackboard),
+                      transitions={"snack_dispensed": "DONE", "snack_not_dispensed": "QUERY"})
+    self.sm.add_state("DONE", DoneState(self.state_publisher_, self.blackboard),
+                      transitions={"finished": "SEARCH"})
+
+    self.StateMachine()
     
 
+  def StateMachine(self):
+    while True:
+      outcome = self.sm()
+      print(f"{outcome=}")      
 
 
-  def nav_target(self):
-    # wait for goal pose message
-    while not self.goal_received:
-      print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] Waiting for goal pose.")
+
+
+
+    # # wait for goal pose message
+    # while not self.goal_received:
+    #   print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] Waiting for goal pose.")
     
-    # reset flag for future goals
-    self.goal_received = False
+    # # reset flag for future goals
+    # self.goal_received = False
 
-    # proceed with navigation if target found
-    target_pose = self.blackboard.get("target_location")
-    if target_pose:
-      self.nav.goToPose(target_pose)
+    # # proceed with navigation if target found
+    # target_pose = self.blackboard.get("target_location")
+    # if target_pose:
+    #   self.nav.goToPose(target_pose)
 
-    while not self.nav.isTaskComplete():
-      feedback = self.nav.getFeedback()
-      # print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] {feedback.distance_remaining=}")
-      msg = String()
-      msg.data = f"[{datetime.datetime.now().strftime('%H:%M:%S')}] APPROACH STATE"
-      self.state_publisher_.publish(msg)
+    # while not self.nav.isTaskComplete():
+    #   feedback = self.nav.getFeedback()
+    #   # print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] {feedback.distance_remaining=}")
+    #   msg = String()
+    #   msg.data = f"[{datetime.datetime.now().strftime('%H:%M:%S')}] APPROACH STATE"
+    #   self.state_publisher_.publish(msg)
 
-    print(f"{self.nav.isTaskComplete()=}")
-    result = self.nav.getResult()
-    if result == TaskResult.SUCCEEDED:
-      print("Goal succeeded!")
-      msg = String()
-      msg.data = f"[{datetime.datetime.now().strftime('%H:%M:%S')}] MOVE TO NEXT STATE"
-      self.state_publisher_.publish(msg)
+    # print(f"{self.nav.isTaskComplete()=}")
+    # result = self.nav.getResult()
+    # if result == TaskResult.SUCCEEDED:
+    #   print("Goal succeeded!")
+    #   msg = String()
+    #   msg.data = f"[{datetime.datetime.now().strftime('%H:%M:%S')}] MOVE TO NEXT STATE"
+    #   self.state_publisher_.publish(msg)
 
 
   def goal_callback(self, msg):
