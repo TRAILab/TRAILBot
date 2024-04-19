abc6ff7d52f58c3d164db2f35527772a0d07c8a0
sacrover <mail.sacrover@gmail.com>
2024-04-19 12:15:23 -0400

    nav2 params update

--- trail_detection_node/trail_detection_node/v2_trailDetectionNode.py
+++ trail_detection_node/trail_detection_node/v2_trailDetectionNode.py
@@ -427,9 +427,9 @@ class trailDetector(Node):
         cv_image = get_rgb_undistorted_img(camera_msg)
         
         undistorted_image = cv_image.copy()
-        # if self.visualize:
-        #     cv2.imshow('Undistorted', cv_image)
-        #     cv2.waitKey(wait_time_max)
+        if self.visualize:
+            cv2.imshow('Undistorted', cv_image)
+            cv2.waitKey(wait_time_max)
 
         #Pre process image to prepare for segmentation model
         cv_image = pre_process_img(cv_image, self.pre_proc_blur_k_size, self.brightness)
@@ -441,16 +441,16 @@ class trailDetector(Node):
         uv_route = None
         if self.visualize:
                         
-            # cv2.imshow('segmentation_ouput',model_pred)
-            # cv2.waitKey(wait_time_max)
+            cv2.imshow('segmentation_ouput',model_pred)
+            cv2.waitKey(wait_time_max)
 
             # Highlight red where is predicted as road
             sign = cv2.cvtColor(model_pred, cv2.COLOR_GRAY2RGB) /255 * 200
             sign = sign.astype(undistorted_image.dtype)  # Convert sign to the data type of undistorted_image
             sign[:, :, :2] = 0
             cv_image = cv2.add(undistorted_image, sign)
-            # cv2.imshow('highlighted_route', cv_image)
-            # cv2.waitKey(wait_time_max)
+            cv2.imshow('highlighted_route', cv_image)
+            cv2.waitKey(wait_time_max)
             if isinstance(pixel_route, np.ndarray):
                 for centre_dot in pixel_route:
                     cv2.circle(cv_image, (centre_dot[0], centre_dot[1]), radius=5, color=(255, 0, 0), thickness=-1)
